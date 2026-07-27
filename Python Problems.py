449e228f79f9c4497cb474e677a34b788bfd4506
NILESH PATIL <nileshpatil06712@gmail.com>
2026-07-28 00:20:32 +0530

    Add files via upload

---
+++ Python Problems.py
@@ -0,0 +1,16 @@
+# Write a Program to input 2 numbers & print their sum.
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("sum:", a + b)
+# WAP to input side of a square & print its area
+side = float(input("Enter the side of the square: "))
+print("Area:", side ** 2)
+# WAP to input 2 floating point numbers & print their average
+num1 = float(input("Enter the first floating point number: "))
+num2 = float(input("Enter the second floating point number: ")) 
+print("Average:", (num1 + num2) / 2)
+# WAP to input 2 int numbers, a and b. 
+# Print True if a is greater than or equal to b. If not print False
+a = int(input("Enter the first integer: "))
+b = int(input("Enter the second integer: "))    
+print(a >= b)
